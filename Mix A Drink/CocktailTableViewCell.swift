a7ea30d7221a8911edfe9aaf962b3500fd2af815
Alex <alexander.danilov1985@gmail.com>
2016-12-11 18:47:47 +0300

    Some of the basic classes and interfaces created

---
+++ Mix A Drink/CocktailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CocktailTableViewCell.swift
+//  Mix A Drink
+//
+//  Created by Alexander on 11.12.16.
+//  Copyright © 2016 Alexander Danilov. All rights reserved.
+//
+
+import UIKit
+
+class CocktailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
